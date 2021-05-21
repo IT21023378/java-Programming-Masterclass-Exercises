0a5ffd885e1f54cf740b842fcf434cd6eb7a5abc
danuja01 <82136975+danuja01@users.noreply.github.com>
2021-05-22 02:40:38 +0530

    Update speedConverter.java

--- Method-Challenges/speedConverter.java
+++ Method-Challenges/speedConverter.java
@@ -17,20 +17,22 @@
 public class SpeedConverter {
   
   public static long toMilesPerHour (double kilometersPerHour) {
-    
-    if (kilometersPerHour < 0) {
-      return -1;
-    }
-    return Math.round(kilometersPerHou / 1.609);
-  }
-  
-  public static void printConversion (double kilometersPerHour) {
-  
-    if (kilometersPerHour < 0) {
-      System.out.println("Invalid Value");
-    }
-    else {
-      long milesPerHour = toMilesPerHour(kilometersPerHour);
-      System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
-  }
+
+         if (kilometersPerHour < 0) {
+             return -1;
+         }
+
+         return  Math.round(kilometersPerHour / 1.609);
+     }
+
+     public static void printConversion (double kilometersPerHour) {
+
+         if (kilometersPerHour < 0) {
+             System.out.println("Invalid Value");
+         }
+         else {
+             long milesPerHour = toMilesPerHour(kilometersPerHour);
+             System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+         }
+
  }
