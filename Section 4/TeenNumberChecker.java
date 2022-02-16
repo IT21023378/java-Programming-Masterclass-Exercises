503b4150ca5a0daa454b34c318bc00c80f8b45c6
Danuja Jayasuriya <82136975+IT21023378@users.noreply.github.com>
2022-02-16 14:16:04 +0530

    Update TeenNumberChecker.java

--- Section 4/TeenNumberChecker.java
+++ Section 4/TeenNumberChecker.java
@@ -31,13 +31,11 @@
 // *********************************************************************************************************************************************
 
 public class TeenNumberChecker {
-   public static boolean hasTeen(int n1,int n2,int n3){
-
-        return (n1 >= 13 && n1 <= 19 || n2 >= 13 && n2 <= 19 || n3 >= 13 && n3 <=19)? true : false;
-
+public static boolean hasTeen(int n1, int n2, int n3){
+        return (n1 >= 13 && n1 <= 19 || n2 >= 13 && n2 <= 19 || n3 >= 13 && n3 <= 19);
     }
-
-    public static boolean isTeen(int n){
-        return (n>=13 && n<=19)? true : false;
+    
+    public static boolean isTeen(int age){
+        return (age >= 13 && age <= 19);
     }
 }
