a082c5a47e61d416a1a80889e1b75dd33484708c
Danuja Jayasuriya <82136975+IT21023378@users.noreply.github.com>
2022-02-16 14:09:40 +0530

    Update EqualSumChecker.java

--- Section 4/EqualSumChecker.java
+++ Section 4/EqualSumChecker.java
@@ -21,7 +21,7 @@
 // *********************************************************************************************************************************************
 
 public class EqualSumChecker {
-  public static boolean hasEqualSum(int n1,int n2,int n3){
-        return (n1 + n2 == n3)?true : false;
+    public static boolean hasEqualSum(int n1, int n2, int n3){
+        return ((n1+n2) == n3);
     }
 }
