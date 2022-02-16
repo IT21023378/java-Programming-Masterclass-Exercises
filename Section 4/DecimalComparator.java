fccd770316c30f5bfe357d35df2d31fb8e7efe8c
Danuja Jayasuriya <82136975+IT21023378@users.noreply.github.com>
2022-02-16 14:03:08 +0530

    Update DecimalComparator.java

--- Section 4/DecimalComparator.java
+++ Section 4/DecimalComparator.java
@@ -27,18 +27,11 @@
 // *********************************************************************************************************************************************
 
 public class DecimalComparator{
-   public static boolean areEqualByThreeDecimalPlaces(double number1 , double number2){
-        number1 = (int) (number1 * 1000);
-        number2 = (int) (number2 * 1000);
+    public static boolean areaEqualByThreeDecimalPlaces(double num1, double num2){
+        num1 = (int)(num1 * 1000);
+        num2 = (int)(num2 * 1000);
+        
+        return (num1 == num2);
 
-        if (number1 == number2) {
-
-            return true;
-
-        }
-        else {
-
-            return false;
-        }
     }
 }
